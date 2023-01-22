3452a2da4c5f87cc6c18f07dd92f29efe236c70d
Tim Jones <timjonez@protonmail.com>
2023-01-21 21:35:19 -0800

    add models for projects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=63)
+    sub_title = models.CharField(max_length=127)
+    live_link = models.URLField(null=True, blank=True)
+    source_link = models.URLField()
+    description = models.TextField()
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=63)
+    image = models.ImageField(null=True, blank=True)
+    projects = models.ManyToManyField(Project, related_name='technologies')
+
+
+class ProjectPicture(models.Model):
+    title = models.CharField(max_length=127)
+    image = models.ImageField()
+    project = models.ForeignKey(
+        Project, related_name='pictures', on_delete=models.CASCADE)
