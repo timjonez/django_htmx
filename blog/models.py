793a7600ec65a641aa2a62e4d4fbd20885491d1e
Tim Jones <timjonez@protonmail.com>
2023-01-21 21:34:41 -0800

    add model for blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    meta_title = models.CharField(max_length=100, blank=True)
+    meta_description = models.CharField(max_length=200, blank=True)
+    og_image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=127)
+    slug = models.SlugField(max_length=127, unique=True)
+    content = models.TextField()
