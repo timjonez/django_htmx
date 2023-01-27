367def5855f9f03f20b254057ead632fad8b35a3
Tim Jones <timjonez@protonmail.com>
2023-01-26 21:54:50 -0800

    add posts to blog page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def blog_list_view(request):
-    return render(request, 'blog/blog_list.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/blog_list.html', {"posts": posts})
