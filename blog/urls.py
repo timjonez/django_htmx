2f612d9406000ea6cea95ee685dc9664b423a80d
Tim Jones <timjonez@protonmail.com>
2023-01-30 20:13:13 -0800

    add post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_list_view, name='blog-list'),
+    path('', views.blog_list_view, name='post-list'),
+    path('post/', views.blog_detail_view, name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def blog_list_view(request):
     posts = Post.objects.all()
     return render(request, 'blog/blog_list.html', {"posts": posts})
+
+def blog_detail_view(request):
+    post = Post.objects.last()
+    return render(request, 'blog/post_detail.html', {"post": post})
