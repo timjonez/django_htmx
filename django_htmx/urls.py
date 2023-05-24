9c8fdbf55815a0baaddc3806601d71cedc2e5ba6
Tim Jones <timjonez@protonmail.com>
2023-05-23 21:54:21 -0700

    add settings and url for media serving

--- django_htmx/urls.py
+++ django_htmx/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("projects.urls", "projects"), namespace="projects")),
     path("blog/", include(("blog.urls", "blog"), namespace="blog")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
